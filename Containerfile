3493df0da3c47496bca0d0bd91eefca855deb81c
renner <renner0@posteo.de>
2026-04-10 18:45:29 +0200

    chore: remove grub and bootupd

--- Containerfile
+++ Containerfile
@@ -7,6 +7,7 @@ FROM ghcr.io/ublue-os/aurora:beta
 
 RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/cache/libdnf5 \
     --mount=type=cache,dst=/var/log \
     --mount=type=tmpfs,dst=/tmp \
     /ctx/build.sh
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,12 +2,18 @@
 
 set -ouex pipefail
 
+dnf config-manager setopt keepcache=1
+
 dnf -y swap bootc https://kojipkgs.fedoraproject.org//packages/bootc/1.15.0/1.fc44/x86_64/bootc-1.15.0-1.fc44.x86_64.rpm
-dnf -y install systemd-boot-unsigned
+
+rm -rf /etc/dnf/protected.d/grub*
+dnf -y do --action=install systemd-boot-unsigned --action=remove bootupd grub2-pc
+
+rm -rf /sysroot/ostree
 
 mkdir -p /usr/lib/dracut/dracut.conf.d/
 printf "systemdsystemconfdir=/etc/systemd/system\nsystemdsystemunitdir=/usr/lib/systemd/system\n" | tee /usr/lib/dracut/dracut.conf.d/30-bootcrew-fix-bootc-module.conf
 printf 'reproducible=yes\nhostonly=no\ncompress=zstd\nadd_dracutmodules+=" bootc "' | tee "/usr/lib/dracut/dracut.conf.d/30-bootcrew-bootc-container-build.conf"
 dracut -v --force "$(find /usr/lib/modules -maxdepth 1 -type d | grep -v -E "*.img" | tail -n 1)/initramfs.img"
 
-rm -rf /sysroot/ostree
+dnf config-manager setopt keepcache=0
